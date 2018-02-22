86e8760156a571f2007910e89f84e1d9b92e360a
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-02-22 21:26:49 +0900

    start porting to C#/.NET Core

---
+++ cpp/SampleIMESharp/SampleIME.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TSF.TypeLib;
+
+namespace SampleIMESharp
+{
+    public class SampleIME : ITfTextInputProcessorEx,
+        ITfThreadMgrEventSink,
+        ITfTextEditSink,
+        ITfKeyEventSink,
+        ITfCompositionSink,
+        ITfDisplayAttributeProvider,
+        ITfActiveLanguageProfileNotifySink,
+        ITfThreadFocusSink,
+        ITfFunctionProvider,
+        ITfFnGetPreferredTouchKeyboardLayout
+    {
+
+    }
+}
