29584511cd7e929cfad9f2f93afd2854b4f58397
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-05-06 11:19:23 +0900

    readonly

--- cpp/SampleIMESharp/Globals.cs
+++ cpp/SampleIMESharp/Globals.cs
@@ -14,16 +14,16 @@ namespace SampleIMESharp
         // CRITICAL_SECTION CS;
         // HFONT defaultlFontHandle;				// Global font object we use everywhere;
 
-        public static string TEXTSERVICE_MODEL = "Apartment";
+        public static readonly string TEXTSERVICE_MODEL = "Apartment";
         // MAKELANGID(LANG_CHINESE, SUBLANG_CHINESE_SIMPLIFIED)
-        public static LangID TEXTSERVICE_LANGID = new LangID((0x02 << 10) | 0x04);
-        public static uint TEXTSERVICE_ICON_INDEX = unchecked((uint)-12);
+        public static readonly LangID TEXTSERVICE_LANGID = new LangID((0x02 << 10) | 0x04);
+        public static readonly uint TEXTSERVICE_ICON_INDEX = unchecked((uint)-12);
 
         //---------------------------------------------------------------------
         // SampleIME CLSID
         //---------------------------------------------------------------------
         // {D2291A80-84D8-4641-9AB2-BDD1472C846B}
-        public static Guid SampleIMECLSID = new Guid(
+        public static readonly Guid SampleIMECLSID = new Guid(
             0xd2291a80,
             0x84d8,
             0x4641,
@@ -34,7 +34,7 @@ namespace SampleIMESharp
         // Profile GUID
         //---------------------------------------------------------------------
         // {83955C0E-2C09-47a5-BCF3-F2B98E11EE8B}
-        public static Guid SampleIMEGuidProfile = new Guid(
+        public static readonly Guid SampleIMEGuidProfile = new Guid(
             0x83955c0e,
             0x2c09,
             0x47a5,
@@ -45,7 +45,7 @@ namespace SampleIMESharp
         // PreserveKey GUID
         //---------------------------------------------------------------------
         // {4B62B54B-F828-43B5-9095-A96DF9CBDF38}
-        public static Guid SampleIMEGuidImeModePreserveKey = new Guid(
+        public static readonly Guid SampleIMEGuidImeModePreserveKey = new Guid(
             0x4b62b54b,
             0xf828,
             0x43b5,
@@ -53,7 +53,7 @@ namespace SampleIMESharp
         );
 
         // {5A08D6C4-4563-4E46-8DDB-65E75C4E73A3}
-        public static Guid SampleIMEGuidDoubleSingleBytePreserveKey = new Guid(
+        public static readonly Guid SampleIMEGuidDoubleSingleBytePreserveKey = new Guid(
             0x5a08d6c4,
             0x4563,
             0x4e46,
@@ -61,7 +61,7 @@ namespace SampleIMESharp
         );
 
         // {175F062E-B961-4AED-A3DF-59F78A02862D}
-        public static Guid SampleIMEGuidPunctuationPreserveKey = new Guid(
+        public static readonly Guid SampleIMEGuidPunctuationPreserveKey = new Guid(
             0x175f062e,
             0xb961,
             0x4aed,
@@ -72,7 +72,7 @@ namespace SampleIMESharp
         // Compartments
         //---------------------------------------------------------------------
         // {101011C5-CF72-4F0C-A515-153019593F10}
-        public static Guid SampleIMEGuidCompartmentDoubleSingleByte = new Guid(
+        public static readonly Guid SampleIMEGuidCompartmentDoubleSingleByte = new Guid(
             0x101011c5,
             0xcf72,
             0x4f0c,
@@ -80,7 +80,7 @@ namespace SampleIMESharp
         );
 
         // {DD321BCC-A7F8-4561-9B61-9B3508C9BA97}
-        public static Guid SampleIMEGuidCompartmentPunctuation = new Guid(
+        public static readonly Guid SampleIMEGuidCompartmentPunctuation = new Guid(
             0xdd321bcc,
             0xa7f8,
             0x4561,
@@ -93,7 +93,7 @@ namespace SampleIMESharp
         //---------------------------------------------------------------------
 
         // {89BE500C-9462-4070-9DB0-B467BB051327}
-        public static Guid SampleIMEGuidLangBarIMEMode = new Guid(
+        public static readonly Guid SampleIMEGuidLangBarIMEMode = new Guid(
             0x89be500c,
             0x9462,
             0x4070,
@@ -101,7 +101,7 @@ namespace SampleIMESharp
         );
 
         // {6A11D9DE-46DB-455B-A257-2EB615746BF4}
-        public static Guid SampleIMEGuidLangBarDoubleSingleByte = new Guid(
+        public static readonly Guid SampleIMEGuidLangBarDoubleSingleByte = new Guid(
             0x6a11d9de,
             0x46db,
             0x455b,
@@ -109,7 +109,7 @@ namespace SampleIMESharp
         );
 
         // {F29C731A-A51E-49FB-8A3C-EE51752912E2}
-        public static Guid SampleIMEGuidLangBarPunctuation = new Guid(
+        public static readonly Guid SampleIMEGuidLangBarPunctuation = new Guid(
             0xf29c731a,
             0xa51e,
             0x49fb,
@@ -117,7 +117,7 @@ namespace SampleIMESharp
         );
 
         // {4C802E2C-8140-4436-A5E5-F7C544EBC9CD}
-        public static Guid SampleIMEGuidDisplayAttributeInput = new Guid(
+        public static readonly Guid SampleIMEGuidDisplayAttributeInput = new Guid(
             0x4c802e2c,
             0x8140,
             0x4436,
@@ -125,7 +125,7 @@ namespace SampleIMESharp
         );
 
         // {9A1CC683-F2A7-4701-9C6E-2DA69A5CD474}
-        public static Guid SampleIMEGuidDisplayAttributeConverted = new Guid(
+        public static readonly Guid SampleIMEGuidDisplayAttributeConverted = new Guid(
             0x9a1cc683,
             0xf2a7,
             0x4701,
@@ -138,7 +138,7 @@ namespace SampleIMESharp
         //---------------------------------------------------------------------
 
         // {84B0749F-8DE7-4732-907A-3BCB150A01A8}
-        public static Guid SampleIMEGuidCandUIElement = new Guid(
+        public static readonly Guid SampleIMEGuidCandUIElement = new Guid(
             0x84b0749f,
             0x8de7,
             0x4732,
